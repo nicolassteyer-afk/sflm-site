f9e2221d515161f198a4eb245155ebfe02b5f85e
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-06-08 18:01:42 +0200

    Animate Strasbourg frieze through scroll

--- src/components/ScrollIconFrieze.tsx
+++ src/components/ScrollIconFrieze.tsx
@@ -8,23 +8,23 @@ const assetBase =
 const icons = [
   {
     alt: "Biere Flam's",
-    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-BIERE-BDX.svg`,
+    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-BIERE-BEIGE.svg`,
   },
   {
     alt: "Flamme Flam's",
-    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-FLAMME-BDX.svg`,
+    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-FLAMME-BEIGE.svg`,
   },
   {
     alt: "Fut Flam's",
-    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-FUT-BDX.svg`,
+    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-FUT-BEIGE.svg`,
   },
   {
     alt: "Planche Flam's",
-    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-PLANCHE-BDX.svg`,
+    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-PLANCHE-BEIGE.svg`,
   },
   {
     alt: "Vin Flam's",
-    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-VIN-BDX.svg`,
+    src: `${assetBase}/2025-09-FLAMS-Valise-Logo_ILLU-VIN-BEIGE.svg`,
   },
 ];
 
@@ -40,72 +40,35 @@ function FriezeIcon({
 }) {
   const { scrollY } = useScroll();
   const x = useTransform(scrollY, [0, 840], [0, horizontalOffsets[index]]);
-  const y = useTransform(scrollY, [0, 840], [verticalOffsets[index], 0]);
+  const y = useTransform(scrollY, [0, 840], [verticalOffsets[index], 650]);
   const rotate = useTransform(scrollY, [0, 840], [-8 + index * 4, 0]);
+  const size = useTransform(scrollY, [0, 840], [78, 96]);
 
   return (
     <motion.img
       alt={icon.alt}
-      className="absolute h-20 w-20 -translate-x-1/2 -translate-y-1/2 object-contain drop-shadow-[0_16px_32px_rgba(0,0,0,.25)] xl:h-24 xl:w-24"
+      className="absolute -translate-x-1/2 -translate-y-1/2 object-contain drop-shadow-[0_16px_32px_rgba(0,0,0,.25)]"
       draggable={false}
       src={icon.src}
-      style={{ x, y, rotate }}
+      style={{ x, y, rotate, height: size, width: size }}
     />
   );
 }
 
 export function ScrollIconFrieze() {
   const { scrollY } = useScroll();
-  const opacity = useTransform(scrollY, [0, 80, 1180, 1420], [1, 1, 1, 0]);
-  const scale = useTransform(scrollY, [0, 900], [0.9, 1]);
+  const opacity = useTransform(scrollY, [0, 80, 1080, 1280], [1, 1, 1, 0]);
+  const scale = useTransform(scrollY, [0, 840], [0.9, 1]);
 
   return (
     <motion.div
       aria-hidden="true"
-      className="pointer-events-none fixed left-1/2 top-1/2 z-[55] hidden h-0 w-0 lg:block"
+      className="pointer-events-none absolute left-1/2 top-[50vh] z-30 hidden h-0 w-0 lg:block"
       style={{ opacity, scale }}
     >
       {icons.map((icon, index) => (
         <FriezeIcon icon={icon} index={index} key={icon.src} />
       ))}
     </motion.div>
-  );
-}
-
-export function HeroIconColumn() {
-  return (
-    <div
-      aria-hidden="true"
-      className="pointer-events-none absolute left-1/2 top-1/2 z-30 hidden -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-5 lg:flex"
-    >
-      {icons.map((icon) => (
-        <img
-          alt=""
-          className="h-16 w-16 object-contain drop-shadow-[0_12px_24px_rgba(0,0,0,.2)] xl:h-20 xl:w-20"
-          draggable={false}
-          key={icon.src}
-          src={icon.src}
-        />
-      ))}
-    </div>
-  );
-}
-
-export function BordeauxIconRow() {
-  return (
-    <div
-      aria-hidden="true"
-      className="pointer-events-none absolute left-1/2 top-1/2 z-30 hidden -translate-x-1/2 -translate-y-1/2 items-center gap-10 lg:flex"
-    >
-      {icons.map((icon) => (
-        <img
-          alt=""
-          className="h-20 w-20 object-contain drop-shadow-[0_14px_28px_rgba(0,0,0,.24)] xl:h-24 xl:w-24"
-          draggable={false}
-          key={icon.src}
-          src={icon.src}
-        />
-      ))}
-    </div>
   );
 }
