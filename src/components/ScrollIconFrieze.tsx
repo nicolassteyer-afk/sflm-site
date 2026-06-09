738d0f49eb5eec12fb36d0a34efdf5bb069841e4
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-06-09 15:06:55 +0200

    Stabilize Strasbourg icon frieze scroll

--- src/components/ScrollIconFrieze.tsx
+++ src/components/ScrollIconFrieze.tsx
@@ -10,9 +10,9 @@ const icons = iconNames.map((name) => ({
   beige: `https://raw.githubusercontent.com/nicolassteyer-afk/sflm-site/main/public/assets/flams/2025-09-FLAMS-Valise-Logo_ILLU-${name}-BEIGE.svg`,
 }));
 
-const verticalOffsets = ["-18rem", "-9rem", "0rem", "9rem", "18rem"];
-const midVerticalOffsets = ["-12rem", "-6rem", "0rem", "6rem", "12rem"];
-const horizontalOffsets = ["-34vw", "-17vw", "0vw", "17vw", "34vw"];
+const verticalOffsets = ["-15rem", "-7.5rem", "0rem", "7.5rem", "15rem"];
+const midVerticalOffsets = ["-9rem", "-4.5rem", "0rem", "4.5rem", "9rem"];
+const horizontalOffsets = ["-30vw", "-15vw", "0vw", "15vw", "30vw"];
 
 function FriezeIcon({
   icon,
@@ -22,15 +22,15 @@ function FriezeIcon({
   index: number;
 }) {
   const { scrollY } = useScroll();
-  const x = useTransform(scrollY, [0, 520, 1120], ["0vw", "0vw", horizontalOffsets[index]]);
-  const y = useTransform(scrollY, [0, 520, 1120], [verticalOffsets[index], midVerticalOffsets[index], "0rem"]);
-  const rotate = useTransform(scrollY, [0, 1120], [-7 + index * 3.5, 0]);
-  const bdxOpacity = useTransform(scrollY, [0, 780, 1020], [1, 1, 0]);
-  const beigeOpacity = useTransform(scrollY, [0, 780, 1020], [0, 0, 1]);
+  const x = useTransform(scrollY, [0, 560, 1180], ["0vw", "0vw", horizontalOffsets[index]]);
+  const y = useTransform(scrollY, [0, 560, 1180], [verticalOffsets[index], midVerticalOffsets[index], "0rem"]);
+  const rotate = useTransform(scrollY, [0, 1180], [-7 + index * 3.5, 0]);
+  const bdxOpacity = useTransform(scrollY, [0, 760, 1040], [1, 1, 0]);
+  const beigeOpacity = useTransform(scrollY, [0, 760, 1040], [0, 0, 1]);
 
   return (
     <motion.div
-      className="absolute h-[clamp(3.2rem,6vw,5.8rem)] w-[clamp(3.2rem,6vw,5.8rem)] -translate-x-1/2 -translate-y-1/2"
+      className="absolute h-[clamp(2.9rem,5.2vw,5.2rem)] w-[clamp(2.9rem,5.2vw,5.2rem)] -translate-x-1/2 -translate-y-1/2"
       style={{ x, y, rotate }}
     >
       <motion.img
@@ -53,14 +53,14 @@ function FriezeIcon({
 
 export function ScrollIconFrieze() {
   const { scrollY } = useScroll();
-  const y = useTransform(scrollY, [0, 520, 1120], ["50vh", "78vh", "134vh"]);
-  const opacity = useTransform(scrollY, [0, 80, 1430, 1660], [1, 1, 1, 0]);
-  const scale = useTransform(scrollY, [0, 1120], [0.82, 1]);
+  const y = useTransform(scrollY, [0, 560, 1180, 1550], ["56vh", "66vh", "42vh", "42vh"]);
+  const opacity = useTransform(scrollY, [0, 120, 1420, 1640], [1, 1, 1, 0]);
+  const scale = useTransform(scrollY, [0, 1180], [0.78, 1]);
 
   return (
     <motion.div
       aria-hidden="true"
-      className="pointer-events-none absolute left-1/2 top-0 z-30 hidden h-0 w-0 -translate-x-1/2 md:block"
+      className="pointer-events-none fixed left-1/2 top-0 z-20 hidden h-0 w-0 -translate-x-1/2 md:block"
       style={{ opacity, scale, y }}
     >
       {icons.map((icon, index) => (
