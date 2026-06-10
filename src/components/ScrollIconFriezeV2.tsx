8b9a742166d3155e0cd81cc9788429d3b2921401
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-06-10 10:24:32 +0200

    Stabilize Strasbourg frieze scroll stop

--- src/components/ScrollIconFriezeV2.tsx
+++ src/components/ScrollIconFriezeV2.tsx
@@ -10,9 +10,9 @@ const icons = iconNames.map((name) => ({
   beige: `https://raw.githubusercontent.com/nicolassteyer-afk/sflm-site/main/public/assets/flams/2025-09-FLAMS-Valise-Logo_ILLU-${name}-BEIGE.svg`,
 }));
 
-const verticalOffsets = ["-17rem", "-8.5rem", "0rem", "8.5rem", "17rem"];
-const midVerticalOffsets = ["-10rem", "-5rem", "0rem", "5rem", "10rem"];
-const horizontalOffsets = ["-28vw", "-14vw", "0vw", "14vw", "28vw"];
+const verticalOffsets = ["-15rem", "-7.5rem", "0rem", "7.5rem", "15rem"];
+const midVerticalOffsets = ["-8rem", "-4rem", "0rem", "4rem", "8rem"];
+const horizontalOffsets = ["-24vw", "-12vw", "0vw", "12vw", "24vw"];
 
 function FriezeIcon({
   icon,
@@ -22,11 +22,11 @@ function FriezeIcon({
   index: number;
 }) {
   const { scrollY } = useScroll();
-  const x = useTransform(scrollY, [0, 520, 980], ["0vw", "0vw", horizontalOffsets[index]]);
-  const y = useTransform(scrollY, [0, 520, 980], [verticalOffsets[index], midVerticalOffsets[index], "0rem"]);
-  const rotate = useTransform(scrollY, [0, 980], [-7 + index * 3.5, 0]);
-  const bdxOpacity = useTransform(scrollY, [0, 650, 900], [1, 1, 0]);
-  const beigeOpacity = useTransform(scrollY, [0, 650, 900], [0, 0, 1]);
+  const x = useTransform(scrollY, [0, 520, 900], ["0vw", "0vw", horizontalOffsets[index]]);
+  const y = useTransform(scrollY, [0, 520, 900], [verticalOffsets[index], midVerticalOffsets[index], "0rem"]);
+  const rotate = useTransform(scrollY, [0, 900], [-6 + index * 3, 0]);
+  const bdxOpacity = useTransform(scrollY, [0, 560, 820], [1, 1, 0]);
+  const beigeOpacity = useTransform(scrollY, [0, 560, 820], [0, 0, 1]);
 
   return (
     <motion.div
@@ -53,9 +53,9 @@ function FriezeIcon({
 
 export function ScrollIconFriezeV2() {
   const { scrollY } = useScroll();
-  const y = useTransform(scrollY, [0, 520, 980], ["58vh", "78vh", "108vh"]);
-  const opacity = useTransform(scrollY, [0, 80, 1180, 1380], [1, 1, 1, 0]);
-  const scale = useTransform(scrollY, [0, 980], [0.82, 1]);
+  const y = useTransform(scrollY, [0, 520, 900], ["58vh", "78vh", "calc(100vh + clamp(4.5rem, 10vh, 7rem))"]);
+  const opacity = useTransform(scrollY, [0, 80, 1080, 1280], [1, 1, 1, 0]);
+  const scale = useTransform(scrollY, [0, 900], [0.82, 1]);
 
   return (
     <motion.div
